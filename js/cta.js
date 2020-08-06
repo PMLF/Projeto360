c771a7dc0585163abf5477fbd41b4cac730c40ba
PMLF <pmlfonseca@outlook.com>
2020-08-06 21:29:19 +0100

    Form fields verification and small effects added.

--- js/cta.js
+++ js/cta.js
@@ -1,14 +1,12 @@
 document.querySelector(".submit").addEventListener('click',
     function (e) {
-        let rule = /([a-zA-Z0-9][a-zA-Z0-9_\-\.]*@[a-zA-Z0-9\.]+[\.][a-zA-Z0-9]+)|((([00]|[+])351)?[0-9]{9})/g;
-        if (rule.test(document.querySelector('.contact').innerHTML)
-                && document.querySelector('.name').innerHTML != '') {
+        let rule = /([a-zA-Z0-9][a-zA-Z0-9_\-\.]*@[a-zA-Z0-9\.]+[\.][a-zA-Z0-9]+)|((([00]|[+])351)?[0-9]{9})/;
+        if (rule.test(document.querySelector('.contact').value)
+                && document.querySelector('.name').value != '') {
             alert("Recebemos o seu contacto!");
         }
         else {
             alert("Por favor preencha os campos corretamente antes de enviar o seu contacto.");
-            alert(document.querySelector('.name').innerHTML != '');
-            alert(rule.test(document.querySelector('.contact').innerHTML));
         }
     }
 );
--- js/docReady.js
+++ js/docReady.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
     // Pointer mask
+    /*
     let leftX = Math.round(0.82 * screen.width);
     let rightX = Math.round(0.93 * screen.width);
     let topY = Math.round(0.07 * screen.height);
     let bottomY = Math.round(0.21 * screen.height);
+    */
 
     $('.hero').mousemove(function(e) {
         let x = e.clientX;
         let y = e.clientY;
       
-        $('.masked').css('-webkit-mask-position-x', x - 600);
-        $('.masked').css('-webkit-mask-position-y', y - 10);
+        $('.masked').css('-webkit-mask-position-x', x - 642);
+        $('.masked').css('-webkit-mask-position-y', y - 76);
         
-        /*if (e.clientX > leftX && e.clientX < rightX
+        /*
+        if (e.clientX > leftX && e.clientX < rightX
             &&
             e.clientY > topY && e.clientY < bottomY)
         {
@@ -22,11 +25,12 @@ $(document).ready(function(){
         else {
             $('.easter-egg').css('display', 'none');
             $('.masked').css('display', 'inline-block');
-        }*/
+        }
+        */
     });
-    
+
     // Countdown
-    let endOfCampaign = new Date("Aug 07, 2020 01:00:00").getTime();
+    let endOfCampaign = new Date("Aug 07, 2020 19:00:00").getTime();
     let x = setInterval(function() {
         let now = new Date().getTime();
 
@@ -47,4 +51,40 @@ $(document).ready(function(){
         document.querySelector(".minutes").innerHTML = timeMeasures[2];
         document.querySelector(".seconds").innerHTML = timeMeasures[3];
     }, 1000);
+
+    // Main title hover
+    let mainTitleSpans = document.querySelectorAll('.main-title span');
+    for (let i = 0; i < mainTitleSpans.length; i++) {
+        mainTitleSpans[i].addEventListener('mouseover',
+            function (e) {
+                for (let j = 0; j < mainTitleSpans.length; j++) {
+                    mainTitleSpans[j].style.color = '#C7161D';
+                    mainTitleSpans[j].style.transition = '0.6s';
+                }
+            }
+        );
+        mainTitleSpans[i].addEventListener('mouseout',
+            function (e) {
+                for (let j = 0; j < mainTitleSpans.length; j++) {
+                    mainTitleSpans[j].style.color = '#000';
+                    mainTitleSpans[j].style.transition = '0.3s';
+                }
+            }
+        );
+    }
+
+    // Bust hover -> hide pointer
+    let masked = document.querySelector('.masked');
+    let pointer = document.querySelector('.pointer');
+    masked.addEventListener('mouseover',
+        function (e) {
+            pointer.style.display = "none";
+        }
+    );
+    masked.addEventListener('mouseout',
+        function (e) {
+            pointer.style.display = "block";
+            masked.style.opacity = 0;
+        }
+    );
 });
