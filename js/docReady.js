374522e2295ec2d0bc4cacc3b21120c2a4cf4bd9
PMLF <pmlfonseca@outlook.com>
2020-08-09 18:17:24 +0100

    Hero refactoring for better HTML semantics and CSS positioning.

--- js/docReady.js
+++ js/docReady.js
@@ -5,37 +5,16 @@ $(document).ready(function(){
         let y = e.clientY;
       
         $('.masked').css('-webkit-mask-position-x', x - 644);
-        $('.masked').css('-webkit-mask-position-y', y + 84);
+        $('.masked').css('-webkit-mask-position-y', y + 105);
     });
-    $(window).scroll(function() {
+    /*$(window).scroll(function() {
+        let y = $('.masked').position().top;
+        console.log(y);
         $('.masked').css('-webkit-mask-position-y', y + $(window).scrollTop()); 
-    });
-
-    // Countdown
-    let endOfCampaign = new Date("Aug 07, 2020 19:00:00").getTime();
-    let x = setInterval(function() {
-        let now = new Date().getTime();
-
-        let distance = endOfCampaign - now;
-
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        let timeMeasures = [days,hours,minutes,seconds];
-        for (let i = 0; i < timeMeasures.length; i++) {
-            timeMeasures[i] = ('0' + timeMeasures[i]).slice(-2);
-        }
-
-        document.querySelector(".days").innerHTML = timeMeasures[0];
-        document.querySelector(".hours").innerHTML = timeMeasures[1];
-        document.querySelector(".minutes").innerHTML = timeMeasures[2];
-        document.querySelector(".seconds").innerHTML = timeMeasures[3];
-    }, 1000);
+    });*/
 
     // Main title hover
-    let mainTitleSpans = document.querySelectorAll('.main-title span');
+    let mainTitleSpans = document.querySelectorAll('.title-text span');
     for (let i = 0; i < mainTitleSpans.length; i++) {
         mainTitleSpans[i].addEventListener('mouseover',
             function (e) {
@@ -56,7 +35,7 @@ $(document).ready(function(){
     }
 
     // Bust hover -> hide pointer
-    let masked = document.querySelector('.masked');
+    /*let masked = document.querySelector('.masked');
     let pointer = document.querySelector('.pointer');
     masked.addEventListener('mouseover',
         function (e) {
@@ -68,5 +47,28 @@ $(document).ready(function(){
             pointer.style.display = "block";
             masked.style.opacity = 0;
         }
-    );
+    );*/
+
+    // Countdown
+    let endOfCampaign = new Date("Aug 22, 2020 00:00:00").getTime();
+    let x = setInterval(function() {
+        let now = new Date().getTime();
+
+        let distance = endOfCampaign - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        let timeMeasures = [days,hours,minutes,seconds];
+        for (let i = 0; i < timeMeasures.length; i++) {
+            timeMeasures[i] = ('0' + timeMeasures[i]).slice(-2);
+        }
+
+        document.querySelector(".days").innerHTML = timeMeasures[0];
+        document.querySelector(".hours").innerHTML = timeMeasures[1];
+        document.querySelector(".minutes").innerHTML = timeMeasures[2];
+        document.querySelector(".seconds").innerHTML = timeMeasures[3];
+    }, 1000);
 });
