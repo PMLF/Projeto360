d14a0137cbd6bc6f579b7e47d3ffad2603d26d9c
PMLF <pmlfonseca@outlook.com>
2020-08-14 19:05:41 +0100

    Mobile version.

--- js/cta.js
+++ js/cta.js
@@ -3,7 +3,10 @@ document.querySelector(".submit").addEventListener('click',
         let rule = /([a-zA-Z0-9][a-zA-Z0-9_\-\.]*@[a-zA-Z0-9\.]+[\.][a-zA-Z0-9]+)|((([00]|[+])351)?[0-9]{9})/;
         if (rule.test(document.querySelector('.contact').value)
                 && document.querySelector('.name').value != '') {
-            alert("Recebemos o seu contacto!");
+            this.value = "OBRIGADO!";
+            document.querySelectorAll("input").forEach(element => {
+                element.disabled = true;
+            });
         }
         else {
             alert("Por favor preencha os campos corretamente antes de enviar o seu contacto.");
